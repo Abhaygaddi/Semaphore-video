45fab35367bb485c1b883485a8872f5bad5d1697
Abhaygaddi <abhay_gaddi@hotmail.com>
2025-04-04 10:11:22 +0530

    Update and rename Update.ps1 to Update.yaml

--- Update.ps1
+++
@@ -1,5 +0,0 @@
-- name: Manually install updates without using Task Scheduler
-  win_shell: |
-    Install-Module -Name PSWindowsUpdate -Force -Scope AllUsers -Confirm:$false
-    Import-Module PSWindowsUpdate
-    Get-WindowsUpdate -AcceptAll -Install -AutoReboot -IgnoreReboot | Out-File C:\temp\update_log.txt
